56418179ab36a38dfe7bcc1902b92ad7cecf5a48
julidiv <julia.dzivina@gmail.com>
2022-09-20 13:40:37 +0200

    Initial Commit

---
+++ SliderUIView/SliderUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderUIViewApp.swift
+//  SliderUIView
+//
+//  Created by Alexander Golub on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SliderUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
