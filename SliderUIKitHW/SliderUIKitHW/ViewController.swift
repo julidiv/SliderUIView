581cb667ffbac9f1a37cde9f6abb46d01498646c
julidiv <julia.dzivina@gmail.com>
2022-09-20 20:03:43 +0200

    kjbkbj

--- SliderUIKitHW/SliderUIKitHW/ViewController.swift
+++ SliderUIKitHW/SliderUIKitHW/ViewController.swift
@@ -2,18 +2,24 @@
 //  ViewController.swift
 //  SliderUIKitHW
 //
-//  Created by Alexander Golub on 20.09.22.
-//
+ 
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
 
+    @IBAction func changeAlhpaSlider(_ sender: UISlider) {
+        _ = Int(sender.value)
 
+        self.slider.thumbTintColor = UIColor(white: 1, alpha: CGFloat(sender.value))
+    }
+    
 }
 
